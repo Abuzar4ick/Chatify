f6f6834948a0f86eba03ec6c1120e2565e47e1a7
Abuzar <abuzarrrakhmonov@gmail.com>
2025-10-26 19:59:02 +0500

    update profile endpoint added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,11 @@
-import { Router } from 'express'
-const router = Router()
-import { login, logout, signup } from '../controllers/auth.controller.js'
+import { Router } from 'express';
+const router = Router();
+import { login, logout, signup, updateProfile } from '../controllers/auth.controller.js';
+import { protectRouter } from '../middleware/auth.middleware.js';
 
-router.post('/signup', signup)
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/logout', logout);
+router.put('/update-profile', protectRouter, updateProfile);
 
-router.post('/login', login)
-
-router.post('/logout', logout)
-
-export default router
+export default router;
