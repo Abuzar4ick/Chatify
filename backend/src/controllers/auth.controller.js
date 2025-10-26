8263b1139dd32197167031bbda0259b270eba7ab
Abuzar <abuzarrrakhmonov@gmail.com>
2025-10-26 14:20:46 +0500

    login section code refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -65,6 +65,10 @@ export const login = async (req, res) => {
   const { email, password } = req.body
 
   try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password are required" });
+    }
+    
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
@@ -81,7 +85,7 @@ export const login = async (req, res) => {
     });
   } catch (error) {
     console.error("Error in loggin controller:", error);
-    res.status(200).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
