6e489fa1b3c6b1ab472727f8c0f6bc0f9470139f
Abuzar <abuzarrrakhmonov@gmail.com>
2025-10-30 19:23:25 +0500

    message routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,8 +1,14 @@
-import { Router } from "express"
-const router = Router()
+import { Router } from "express";
+const router = Router();
+import { getAllContacts, getMessagesByUserId, sendMessage, getChatPartners } from '../controllers/message.controller.js';
+import { protectRouter } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
-router.get('/send', (req, res) => {
-    res.send('Send message endpoint')
-})
+router.use(arcjetProtection, protectRouter);
 
-export default router
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
+
+export default router;
