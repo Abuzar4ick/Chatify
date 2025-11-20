b12e4be39a2de1fab123166c410cb22dfe3862ca
Abuzar <abuzarrrakhmonov@gmail.com>
2025-11-20 17:19:07 +0500

    sidebar completed

--- frontend/src/components/ActiveTabSwitch.jsx
+++ frontend/src/components/ActiveTabSwitch.jsx
@@ -1,8 +1,22 @@
+import { useChatStore } from "../store/useChatStore"
 
 function ActiveTabSwitch() {
+  const { activeTab, setActiveTab } = useChatStore();
+
   return (
-    <div>
-      ActiveTabSwitch
+    <div className="tabs tabs-boxed bg-transparent p-2 m-2">
+      <button
+        onClick={() => setActiveTab("chats")}
+        className={`tab w-[50%] rounded-xl ${
+          activeTab === "chats" ? "bg-cyan-500/20 text-cyan-400" : "text-slate-400"
+        }`}
+        >Chats</button>
+      <button
+        onClick={() => setActiveTab("contacts")}
+        className={`tab w-[50%] rounded-xl ${
+          activeTab === "contacts" ? "bg-cyan-500/20 text-cyan-400" : "text-slate-400"
+        }`}
+      >Contacts</button>
     </div>
   )
 }
